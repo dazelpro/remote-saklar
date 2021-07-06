06112d7009b0beca9ae4d916ebee2f14a9f5ad4c
dazelpro <dazelpro@gmail.com>
2021-07-06 21:27:27 +0700

    + user service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { UserService } from './cores/user.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'remote-saklar';
+
+    title = 'remote-saklar';
+
+    constructor(
+        public data: UserService
+    ) {
+        this.data.getProfile();
+        console.log(environment.versiApp);
+    }
 }
