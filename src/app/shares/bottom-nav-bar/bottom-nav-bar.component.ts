2af54ab990258a044a2f2f0f7ac8b9bfc0c6ce8b
dazelpro <dazelpro@gmail.com>
2021-07-07 09:45:46 +0700

    in active page

--- src/app/shares/bottom-nav-bar/bottom-nav-bar.component.ts
+++ src/app/shares/bottom-nav-bar/bottom-nav-bar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-bottom-nav-bar',
-  templateUrl: './bottom-nav-bar.component.html',
-  styleUrls: ['./bottom-nav-bar.component.css']
+    selector: 'app-bottom-nav-bar',
+    templateUrl: './bottom-nav-bar.component.html',
+    styleUrls: ['./bottom-nav-bar.component.css']
 })
 export class BottomNavBarComponent implements OnInit {
 
-  constructor() { }
+    isUrl;
+    isPage;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private router : Router,
+    ) { }
+
+    ngOnInit(): void {
+        this.isUrl = this.router.url;
+        if (this.isUrl.match(/dashboard.*/)) {
+            this.isPage = 'Dashboard'
+        } else if (this.isUrl.match(/device.*/)) {
+            this.isPage = 'Device'
+        } else if (this.isUrl.match(/profile.*/)) {
+            this.isPage = 'Profile'
+        }
+        console.log(this.isPage)
+    }
 
 }
