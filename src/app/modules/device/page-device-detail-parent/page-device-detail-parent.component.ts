bd5fd4c8af10a2dbb3004dd5cbb2571f19f04a3f
dazelpro <dazelpro@gmail.com>
2021-07-08 21:13:31 +0700

    get param url

--- src/app/modules/device/page-device-detail-parent/page-device-detail-parent.component.ts
+++ src/app/modules/device/page-device-detail-parent/page-device-detail-parent.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-page-device-detail-parent',
-  templateUrl: './page-device-detail-parent.component.html',
-  styleUrls: ['./page-device-detail-parent.component.css']
+    selector: 'app-page-device-detail-parent',
+    templateUrl: './page-device-detail-parent.component.html',
+    styleUrls: ['./page-device-detail-parent.component.css']
 })
 export class PageDeviceDetailParentComponent implements OnInit {
 
-  constructor() { }
+    id
+    
+    constructor(
+        private activatedRoute: ActivatedRoute,
+    ) { 
+        if (activatedRoute.snapshot.url[0]){
+            this.id = activatedRoute.snapshot.url[0]["path"];
+        }
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        console.log(this.id)
+    }
 
 }
