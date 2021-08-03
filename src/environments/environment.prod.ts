cc1391e27090f9b32f515f83f893f1a1696367b4
dazelpro <dazelpro@gmail.com>
2021-08-03 15:37:05 +0700

    deploy 1.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  versiApp: "1.0",
+    urlApi: "https://apps.dazelpro.com",
+    googleAuthKey: "53061255389-klc8do5ic85a785g4elv4ip15l3i8o6p.apps.googleusercontent.com",
+    firebaseConfig : {
+        apiKey: "AIzaSyBhQWlNfwTNdGvI7rIqy5X71zfK2zKrNJw",
+        authDomain: "remote-saklar.firebaseapp.com",
+        databaseURL: "https://remote-saklar-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "remote-saklar",
+        storageBucket: "remote-saklar.appspot.com",
+        messagingSenderId: "125869345002",
+        appId: "1:125869345002:web:476be7ed0f602697882f93"
+    }
 };
