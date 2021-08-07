e35679d3c8db8fefd7c3ea578b420f56d64e38e3
dazelpro <dazelpro@gmail.com>
2021-08-07 18:32:31 +0700

    verionering pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate } from '@angular/service-worker';
 import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
 import { environment } from 'src/environments/environment';
 import { UserService } from './cores/user.service';
@@ -17,8 +19,16 @@ export class AppComponent {
     title = 'remote-saklar';
 
     constructor(
-        public data: UserService
+        public data: UserService,
+        private swUpdate: SwUpdate,
+        private _snackBar: MatSnackBar
     ) {
+        this.swUpdate.available.subscribe(event => {
+            const snack = this._snackBar.open("Versi terbaru telah tersedia", "Update");
+            snack.onAction().subscribe(() => {
+                window.location.reload();
+            })
+        });
         console.log(environment.versiApp);
     }
 }
