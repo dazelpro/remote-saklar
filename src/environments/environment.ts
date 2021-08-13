8084d6e5ba724974d47292f3d497bc10bae1098c
dazelpro <dazelpro@gmail.com>
2021-08-13 23:18:09 +0700

    SEO

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    versiApp: "1.5",
+    versiApp: "1.6",
     urlApi: "http://localhost:8000",
     googleAuthKey: "53061255389-klc8do5ic85a785g4elv4ip15l3i8o6p.apps.googleusercontent.com",
     firebaseConfig : {
