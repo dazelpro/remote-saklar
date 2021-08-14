4489be771183d6c099dec47c82a02c6e47c7c79a
dazelpro <dazelpro@gmail.com>
2021-08-14 14:31:54 +0700

    adding notif offline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SwUpdate } from '@angular/service-worker';
 import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
+import { ConnectionService } from 'ng-connection-service';
 import { environment } from 'src/environments/environment';
 import { UserService } from './cores/user.service';
 
@@ -10,18 +11,20 @@ import { UserService } from './cores/user.service';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     animations: [
-        fadeInOnEnterAnimation(),
-        fadeOutOnLeaveAnimation()
+        fadeInOnEnterAnimation({duration:250}),
+        fadeOutOnLeaveAnimation({duration:250})
     ]
 })
 export class AppComponent {
 
+    isConnected = true;
     title = 'remote-saklar';
 
     constructor(
         public data: UserService,
         private swUpdate: SwUpdate,
-        private _snackBar: MatSnackBar
+        private _snackBar: MatSnackBar,
+        private connectionService: ConnectionService,
     ) {
         this.swUpdate.available.subscribe(event => {
             const snack = this._snackBar.open("Versi terbaru telah tersedia", "Update");
@@ -29,6 +32,19 @@ export class AppComponent {
                 window.location.reload();
             })
         });
+        this.connectionService.monitor().subscribe(isConnected => {
+            this.isConnected = isConnected;
+
+            if (this.isConnected) {
+                console.log(this.isConnected)
+            } else {
+                this._snackBar.open('Koneksi internet terganggu', '', {
+                    duration: 3000,
+                    panelClass: ['mat-snackbar', 'mat-primary']
+                });
+            }
+
+        })
         console.log(environment.versiApp);
     }
 }
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -4,9 +4,10 @@ import { PageProfileComponent } from './page-profile/page-profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { MaterialModule } from 'src/app/material.module';
 import { SharesModule } from 'src/app/shares/shares.module';
+import { DialogConfirmLogoutComponent } from './dialog-confirm-logout/dialog-confirm-logout.component';
 
 @NgModule({
-    declarations: [PageProfileComponent],
+    declarations: [PageProfileComponent, DialogConfirmLogoutComponent],
     imports: [
         CommonModule,
         ProfileRoutingModule,
